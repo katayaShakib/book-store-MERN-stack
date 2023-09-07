683783881e2bb7c7a2e6c84525a5af02f2433add
Shakib KATAYA <kataya.shakib@gmail.com>
2023-09-07 17:37:34 +0300

    Create pages & Add thier routes to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import CreateBook from "./pages/CreateBook";
+import DeleteBook from "./pages/DeleteBook";
+import EditBook from "./pages/EditBook";
+import Home from "./pages/Home";
+import ShowBook from "./pages/ShowBook";
 
 const App = () => {
-  return <div className="bg-red-400 text-white">App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/books/create" element={<CreateBook></CreateBook>}></Route>
+      <Route path="/books/details/:id" element={<ShowBook></ShowBook>}></Route>
+      <Route path="/books/edit/:id" element={<EditBook></EditBook>}></Route>
+      <Route
+        path="/books/delete/:id"
+        element={<DeleteBook></DeleteBook>}
+      ></Route>
+    </Routes>
+  );
 };
 
 export default App;
