991608297ef2e13eea4a3bc66226afff22db0a3d
Shakib KATAYA <kataya.shakib@gmail.com>
2023-09-08 11:07:18 +0300

    Enable cors

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import bookRoute from "./routes/bookroute.js";
 import "dotenv/config";
+import cors from "cors";
 
 const PORT = process.env.PORT;
 const MONGODB_URL = process.env.MONGODB_URL;
@@ -10,6 +11,15 @@ const app = express();
 
 // middleware for parsing request body
 app.use(express.json());
+
+/* app.use(cors()); */
+/* app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    AllowedHeaders: "Content-Type",
+  })
+); */
 
 app.get("/", (req, res) => {
   return res.status(234).send("Welcome to Book Store!");
