5f348feffc79cd3ee4b248ea12bd34ec53f367d9
Shakib KATAYA <kataya.shakib@gmail.com>
2023-09-11 10:19:41 +0300

    Create Books'Table, Card & SingleCrad comp.

---
+++ frontend/src/components/home/BooksCards.jsx
@@ -0,0 +1,13 @@
+import BooksSingleCard from "./BooksSingleCard";
+
+const BooksCards = ({ books }) => {
+  return (
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {books.map((book) => (
+        <BooksSingleCard key={book._id} book={book}></BooksSingleCard>
+      ))}
+    </div>
+  );
+};
+
+export default BooksCards;
